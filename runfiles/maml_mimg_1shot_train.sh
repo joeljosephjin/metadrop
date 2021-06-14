3c7be81c62e5047a2a8780871ffdd3875bc7c1c9
Joel Joseph <joeljosephjin@gmail.com>
2021-06-14 15:52:02 +0530

    split sh files

--- runfiles/maml_mimg_1shot_train.sh
+++ runfiles/maml_mimg_1shot_train.sh
@@ -13,17 +13,4 @@ python main.py \
   --n_test_mc_samp 1 \
   --maml
 
-python main.py \
-  --savedir './results/maml/mimgnet_1shot' \
-  --dataset 'mimgnet' \
-  --mode 'meta_test' \
-  --gpu_id 0 \
-  --metabatch 5 \
-  --inner_lr 0.01 \
-  --n_steps 5 \
-  --way 5 \
-  --shot 1 \
-  --query 15 \
-  --meta_lr 1e-4 \
-  --n_test_mc_samp 1 \
-  --maml
+
