63540bee13023fe660596ff014b79ac7caa4a2da
uname+joeljosephjin <uname+@gmail.com>
2021-08-05 09:20:44 +0000

    more

--- model.py
+++ model.py
@@ -3,21 +3,13 @@ from layers import *
 class MetaDropout:
     def __init__(self, args):
         self.dataset = args.dataset
-        if self.dataset == 'omniglot':
-            self.xdim, self.input_channel = 28, 1
-            self.n_channel = 64 # channel dim of conv layers
-        elif self.dataset == 'mimgnet':
-            self.xdim, self.input_channel = 84, 3
-            self.n_channel = 32
+        self.xdim, self.input_channel = 28, 1
+        self.n_channel = 64 # channel dim of conv layers
 
         self.way = args.way # num of classes per each episode
         self.n_steps = args.n_steps # num of inner gradient steps
         self.metabatch = args.metabatch # metabatch size
         self.inner_lr = args.inner_lr # inner-gradient stepsize
-
-        # number of MC samples to evaluate the expected inner-step loss
-        # over the input-dependent noise distribution
-        self.n_test_mc_samp = args.n_test_mc_samp
 
         # whether to convert this model back to the base MAML or not
         self.maml = args.maml
@@ -31,25 +23,16 @@ class MetaDropout:
                 'xte': tf.placeholder(tf.float32, xshape, name='xte'),
                 'yte': tf.placeholder(tf.float32, yshape, name='yte')}
 
-        # param initializers
-        self.conv_init = tf.truncated_normal_initializer(stddev=0.02)
-        self.fc_init = tf.random_normal_initializer(stddev=0.02)
-        self.zero_init = tf.zeros_initializer()
-
     # main model param.
     def get_theta(self, reuse=None):
         with tf.variable_scope('theta', reuse=reuse):
             theta = {}
             for l in [1,2,3,4]:
                 indim = self.input_channel if l == 1 else self.n_channel
-                theta['conv%d_w'%l] = tf.get_variable('conv%d_w'%l,
-                        [3, 3, indim, self.n_channel], initializer=self.conv_init)
-                theta['conv%d_b'%l] = tf.get_variable('conv%d_b'%l,
-                        [self.n_channel], initializer=self.zero_init)
-            theta['dense_w'] = tf.get_variable('dense_w',
-                    [self.n_channel, self.way], initializer=self.fc_init)
-            theta['dense_b'] = tf.get_variable('dense_b',
-                    [self.way], initializer=self.zero_init)
+                theta['conv%d_w'%l] = tf.get_variable('conv%d_w'%l, [3, 3, indim, self.n_channel])
+                theta['conv%d_b'%l] = tf.get_variable('conv%d_b'%l, [self.n_channel])
+            theta['dense_w'] = tf.get_variable('dense_w', [self.n_channel, self.way])
+            theta['dense_b'] = tf.get_variable('dense_b', [self.way])
             return theta
 
     # noise function param.
@@ -58,13 +41,10 @@ class MetaDropout:
             phi = {}
             for l in [1,2,3,4]:
                 indim = self.input_channel if l == 1 else self.n_channel
-                phi['conv%d_w'%l] = tf.get_variable('conv%d_w'%l,
-                        [3, 3, indim, self.n_channel], initializer=self.conv_init)
-                phi['conv%d_b'%l] = tf.get_variable('conb%d_b'%l,
-                        [self.n_channel], initializer=self.zero_init)
+                phi['conv%d_w'%l] = tf.get_variable('conv%d_w'%l, [3, 3, indim, self.n_channel], initializer=self.conv_init)
+                phi['conv%d_b'%l] = tf.get_variable('conb%d_b'%l, [self.n_channel], initializer=self.zero_init)
             factor = 5*5 if self.dataset == 'mimgnet' else 1
-            single_w = tf.get_variable('dense_w', [factor*self.n_channel, 1],
-                    initializer=self.fc_init)
+            single_w = tf.get_variable('dense_w', [factor*self.n_channel, 1], initializer=self.fc_init)
             single_b = tf.get_variable('dense_b', [1], initializer=self.zero_init)
             phi['dense_w'] = tf.tile(single_w, [1, self.way])
             phi['dense_b'] = tf.tile(single_b, [self.way])
@@ -78,8 +58,7 @@ class MetaDropout:
         for l in [1,2,3,4]:
             wt, bt = theta['conv%d_w'%l], theta['conv%d_b'%l]
             wp, bp = phi['conv%d_w'%l], phi['conv%d_b'%l]
-            x = conv_block(x, wt, bt, wp, bp, sample=sample,
-                    bn_scope='conv%d_bn'%l, maml=self.maml)
+            x = conv_block(x, wt, bt, wp, bp, sample=sample, bn_scope='conv%d_bn'%l, maml=self.maml)
 
         # final dense layer --> additive noise
         wt, bt = theta['dense_w'], theta['dense_b']
@@ -97,12 +76,8 @@ class MetaDropout:
         for i in range(self.n_steps):
             inner_loss = []
 
-            # evaluate the expected loss over input-dependent noise distribution with MC approx.
-            # if meta-training then we sample once for efficiency.
-            # if meta-testing then we sample as much as possible (e.g. 30) for accuracy.
-            for j in range(1 if training else self.n_test_mc_samp):
-                inner_logits = self.forward(xtr, theta, phi, sample=True)
-                inner_loss.append(cross_entropy(inner_logits, ytr))
+            inner_logits = self.forward(xtr, theta, phi, sample=True)
+            inner_loss.append(cross_entropy(inner_logits, ytr))
             inner_loss = tf.reduce_mean(inner_loss)
 
             # compute inner-gradient
@@ -110,8 +85,7 @@ class MetaDropout:
             gradients = dict(zip(theta.keys(), grads))
 
             # perform the current gradient step
-            theta = dict(zip(theta.keys(),
-                [theta[key] - self.inner_lr * gradients[key] for key in theta.keys()]))
+            theta = dict(zip(theta.keys(), [theta[key] - self.inner_lr * gradients[key] for key in theta.keys()]))
 
         logits = self.forward(xte, theta, phi, sample=False)
         cent = cross_entropy(logits, yte)
@@ -127,11 +101,7 @@ class MetaDropout:
         get_single_test = lambda inputs: self.get_loss_single(inputs, False, reuse=True)
         get_single = get_single_train if training else get_single_test
 
-        cent, acc \
-                = tf.map_fn(get_single,
-                        elems=(xtr, ytr, xte, yte),
-                        dtype=(tf.float32, tf.float32),
-                        parallel_iterations=self.metabatch)
+        cent, acc = tf.map_fn(get_single, elems=(xtr, ytr, xte, yte), dtype=(tf.float32, tf.float32), parallel_iterations=self.metabatch)
 
         # return the output
         net = {}
